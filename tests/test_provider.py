8c5683ac097af1ba81997fbbcbeefab828076a45
erik <erik@q32.com>
2020-01-31 08:48:02 -0500

    override gq ... unreliable

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -1,4 +1,16 @@
-import sys
-import pytest
-from cloudsync_onedrive import OneDriveProvider
+"""Imported test suite"""
+
+import io
 from cloudsync.tests import *
+
+
+test_report_info = None
+
+
+def test_report_info_od(provider):
+    temp_name = provider.temp_name()
+    provider.get_quota()["used"]
+    provider.create(temp_name, io.BytesIO(b"test" * 1000))
+    pinfo2 = provider.get_quota()
+    assert pinfo2['used'] > 0
+    assert pinfo2['limit'] > 0
