4987267363875f748c5bd0b7d2d259dc86a1a6b0
erik <erik@q32.com>
2020-03-11 16:09:32 -0400

    ver

--- cloudsync_onedrive.py
+++ cloudsync_onedrive.py
@@ -36,7 +36,7 @@ from cloudsync.registry import register_provider
 from cloudsync.utils import debug_sig, memoize
 
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 
 class OneDriveFileDoneError(Exception):
