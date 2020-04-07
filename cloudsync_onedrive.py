eef4958c48888bf8daba680d31d0fe9fa6e256eb
erik <erik@q32.com>
2020-04-07 09:19:43 -0400

    verbump

--- cloudsync_onedrive.py
+++ cloudsync_onedrive.py
@@ -36,7 +36,7 @@ from cloudsync.registry import register_provider
 from cloudsync.utils import debug_sig, memoize
 
 
-__version__ = "0.1.10"
+__version__ = "0.1.11"
 
 
 class OneDriveFileDoneError(Exception):
