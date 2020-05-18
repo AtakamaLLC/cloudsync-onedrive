1f3b1626b42d338fdbf4f5b92c5cee8c7be91ee8
earonesty <erik@q32.com>
2020-05-18 09:36:00 -0400

    Update cloudsync_onedrive.py

--- cloudsync_onedrive.py
+++ cloudsync_onedrive.py
@@ -38,7 +38,7 @@ from cloudsync.utils import debug_sig, memoize
 import quickxorhash
 
 
-__version__ = "0.1.19" # pragma: no cover
+__version__ = "0.1.20" # pragma: no cover
 
 SOCK_TIMEOUT = 180
 
