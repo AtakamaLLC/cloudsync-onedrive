058d07cf541715970d51c0fa79ebda48cf33eb83
Michael Krebs <michael@vidaidentity.com>
2020-11-08 16:59:34 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cloudsync-onedrive',
+    version='2.2.0a',
+    packages=['tests'],
+    url='',
+    license='',
+    author='Atakama',
+    author_email='',
+    description=''
+)
