724bfc2a423e0f9073d90c4a708e6c6d3cb7a2fb
Ilya Ayzenberg <10301392+howufiger7@users.noreply.github.com>
2021-05-03 12:15:11 -0400

    bump ver

--- cloudsync_onedrive.py
+++ cloudsync_onedrive.py
@@ -40,7 +40,7 @@ from cloudsync.utils import debug_sig, memoize
 
 import quickxorhash
 
-__version__ = "3.1.4"  # pragma: no cover
+__version__ = "3.1.5"  # pragma: no cover
 
 
 SOCK_TIMEOUT = 180
