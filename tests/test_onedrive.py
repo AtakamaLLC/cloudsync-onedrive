e405df9e4ec464ef7973409fb54330cce3fbc8f9
Ilya Ayzenberg <10301392+howufiger7@users.noreply.github.com>
2021-07-07 11:56:30 -0400

    lint

--- tests/test_onedrive.py
+++ tests/test_onedrive.py
@@ -5,7 +5,7 @@ import json
 import logging
 import re
 import typing
-from unittest.mock import patch, call
+from unittest.mock import patch, call, MagicMock
 
 import pytest
 import requests
@@ -698,9 +698,10 @@ def test_cursor_error():
             list(odp.events())
 
     def return_400(*args, **kwargs):
-        resp = requests.Response()
+        resp = MagicMock()
         resp.status_code = 400
         resp.json = lambda **kwargs: {"error": {"message": "malformed sync token", "code": ErrorCode.InvalidRequest}}
+
         return resp
 
     with patch.object(odp._http.session, "request", return_400), patch.object(odp, "_check_ns", return_value=True):
