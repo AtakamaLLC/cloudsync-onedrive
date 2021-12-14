f8710f3a59dc82f8fcabff3de8b57c1670570f79
Ilya Ayzenberg <10301392+howufiger7@users.noreply.github.com>
2021-12-14 15:39:48 -0500

    update cloudsync dependency

--- cloudsync_onedrive.py
+++ cloudsync_onedrive.py
@@ -40,7 +40,7 @@ from cloudsync.utils import debug_sig, memoize
 
 import quickxorhash
 
-__version__ = "3.1.9"  # pragma: no cover
+__version__ = "3.1.10"  # pragma: no cover
 
 
 SOCK_TIMEOUT = 180
